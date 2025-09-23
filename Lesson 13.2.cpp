66a8c7a3f578ae3457478766a4db44aca7471b38
Ritgard <94445962+Ritgard@users.noreply.github.com>
2025-09-23 21:54:31 +0300

    Add files via upload

---
+++ Lesson 13.2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct BankAccount
+{
+	int numberAccounts{};
+	string accountName{};
+	float countMoney{};
+};
+
+void changeBankAccount(BankAccount& b, float newSum);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	float newSum{};
+	BankAccount first;
+	cout << "Введите номер счета: ";
+	cin >> first.numberAccounts;
+	cin.ignore();
+	cout << "Введите имя владельца: ";
+	getline(cin, first.accountName);
+	cout << "Введите баланс: ";
+	cin >> first.countMoney;
+	cout << "Введите новый баланс: ";
+	cin >> newSum;
+	changeBankAccount(first, newSum);
+	cout << "Ваш счет: " << first.accountName << ", " << first.numberAccounts << ", " << first.countMoney << endl;
+
+	return EXIT_SUCCESS;
+}
+void changeBankAccount(BankAccount& b, float newSum)
+{
+	b.countMoney += newSum;
+}
