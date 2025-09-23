7d4c0846342d8cb959df652b91c6e7b1fc00f22e
Ritgard <94445962+Ritgard@users.noreply.github.com>
2025-09-23 22:27:48 +0300

    Add files via upload

---
+++ Lesson 13.3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct CityInfo
+{
+	string nameCity{};
+	string nameStreet{};
+	int numberHouse{};
+	int numberApartment{};
+	int index{};
+};
+
+void viewStruct(const CityInfo& a);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	CityInfo one{ "Москва", "Арбат", 123, 55555, 9922831 };
+	CityInfo two{ "Санкт-Петербург", "Невский проспект", 0, 1, 1983291 };
+	viewStruct(one);
+	cout << endl;
+	viewStruct(two);
+
+	return EXIT_SUCCESS;
+}
+void viewStruct(const CityInfo& a)
+{
+	cout << "Город: " << a.nameCity << endl;
+	cout << "Улица: " << a.nameStreet << endl;
+	cout << "Номер дома: " << a.numberHouse << endl;
+	cout << "Номер квартиры: " << a.numberApartment << endl;
+	cout << "Индекс: " << a.index << endl;
+}
